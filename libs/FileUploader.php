982fc6a88552fd72d24587a3706df7d8b7e00d61
= <vmataba0@gmail.com>
2019-06-27 09:22:14 +0300

    add multipl files upload funnctionality

--- libs/FileUploader.php
+++ libs/FileUploader.php
@@ -17,12 +17,12 @@ namespace libs;
 class FileUploader {
 
     public static function uploadFile($inputFileName, $destinationPath, $includeTimeStamp) {
-       
-        if ($inputFileName === null){
+
+        if ($inputFileName === null) {
             $inputFileName = 'nativeFileInput';
         }
 
-       
+
         if (isset($_FILES[$inputFileName])) {
             $fileName = $_FILES[$inputFileName]['name'];
 
@@ -53,6 +53,66 @@ class FileUploader {
                 'fileInput' => $_FILES[$inputFileName]
             ];
         }
+    }
+
+     public static function uploadMultipleFiles($inputFileName, $destinationPath, $includeTimeStamp) {
+        if (isset($_FILES[$inputFileName])) {
+
+
+
+            $files = $_FILES['files'];
+            //Original files' names
+            $fileNames = $files['name'];
+            //File Types
+            $fileTypes = $files['type'];
+            //Temporary file names
+            $fileTempNames = $files['tmp_name'];
+            //File sizes
+            $fileSizes = $files['size'];
+            //File Errors
+            $fileErrors = $files['error'];
+
+            //Has final response for each file
+            $successResponse = [];
+
+            for ($index = 0; $index < sizeof($fileNames); $index++) {
+
+
+                //Start
+                $fileName = $fileNames[$index];
+
+                $fileExtension = explode('.', $fileName)[sizeof(explode('.', $fileName)) - 1];
+
+                $finalFileName = $fileName;
+
+                if ($includeTimeStamp) {
+                    $finalFileName = explode('.', $fileName)[0] . '-' . date('y-m-d h:i:s') . '.' . $fileExtension;
+                }
+
+                $completePath = $destinationPath . $finalFileName;
+
+                if (move_uploaded_file($fileTempNames[$index], $completePath)) {
+
+
+                    array_push($successResponse, [
+                        'type' => 1,
+                        'typeDescription' => 'Success',
+                        'completePath' => $completePath,
+                        'usedName' => $finalFileName,
+                        'fileInput' => [
+                            'name' => $fileNames[$index],
+                            'type' => $fileTypes[$index],
+                            'tempName' => $fileTempNames[$index],
+                            'size' => $fileSizes[$index],
+                            'error' => $fileErrors[$index]
+                        ]
+                    ]);
+                } else {
+                    //Handling upload errors
+                }
+            }
+        }
+        return $successResponse;
     }
 
     public static function widget() {
